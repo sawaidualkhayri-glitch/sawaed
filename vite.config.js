c11a287821294f2db6a10e79e0f4b1a1e6c70299
MohamedALNatsh <104651138+mohamedalnatsh@users.noreply.github.com>
2026-08-24 20:42:37 +0300

    loaz-3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev
 export default defineConfig({
+  server: {
+    headers: {
+      "Content-Security-Policy": "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://www.googletagmanager.com https://*.firebaseio.com https://apis.google.com https://*.googleapis.com; script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' blob: https://www.googletagmanager.com https://*.firebaseio.com https://apis.google.com https://*.googleapis.com; worker-src 'self' blob:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; connect-src 'self' blob: https://*.workers.dev https://sawaed.hamodemsg.workers.dev https://*.firebaseio.com wss://*.firebaseio.com https://*.googleapis.com https://firestore.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://identitytoolkit.googleapis.com https://drive.google.com; frame-src 'self' https://*.firebaseapp.com https://apis.google.com https://*.google.com; img-src 'self' data: blob: https://*.googleusercontent.com https://www.gstatic.com https:; object-src 'none';"
+    }
+  },
   plugins: [
     react(),
     VitePWA({
